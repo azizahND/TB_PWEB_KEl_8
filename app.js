e54288d9c125d80d429bf0d77c9a148624cba20a
azizahND <azizahnovidelfianti@gmail.com>
2024-05-12 21:52:59 +0700

    selesai

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-
 const express = require('express');
+const path = require('path'); 
 const app = express();
-const port = 3030;
+const port = 8080;
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.send('Halo dari Express.js!');
+  res.send('selamat datang');
 });
 
 app.get('/profilmahasiswa', (req, res) => {
   res.render('profile-m'); 
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
